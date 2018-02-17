fe09c80e3fe19ef5600cd00c454d8c5b1602a3ff
Curtis Haig Gallagher <chg@localhost.localdomain>
2018-02-17 17:16:58 -0500

    updates

--- shell/shell.c
+++ shell/shell.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 void parse(char *args, char **args_parsed){
-
   while (*args != '\0'){
     while (*args == ' ' || *args == '\t' || *args == '\n'){
       *args++ = '\0';
@@ -14,10 +15,25 @@ void parse(char *args, char **args_parsed){
       args++;
     }
   }
-  *args_parsed = '\0';                
+  *args_parsed = '\0';
 }
 
-int main(int argc, char *argv){
+void execute(char **args){
+  int pid = fork();
+  int status;
+  if(pid < 0){
+	printf("Failed to fork");
+	exit(1);
+  }else if(pid > 0){
+	while(wait(&status) != pid);
+  }else{
+	if(execvp(*args, args) < 0){
+	  printf("Failed to execute");
+	  exit(1);
+	}
+  }
+}
+int main(int argc, char **argv){
   char args[1024];
   char *args_parsed[64];
   /*
@@ -30,10 +46,13 @@ int main(int argc, char *argv){
   printf("prompt> ");
   fgets(args, 1024, stdin);
   parse(args, args_parsed);
+  /*
   while(*args_parsed != NULL){
+
     printf("%s\n", *args_parsed);
     args_parsed++;
 
   }
+  */
   return 0;
 }
