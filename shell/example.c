5f726b37dca75a897fd4adf50cd1eb077f2030e2
Curtis Haig Gallagher <chg@localhost.localdomain>
2018-02-21 15:29:07 -0500

    pseudo code updates

--- shell/example.c
+++ shell/example.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -7,12 +8,12 @@
 
 void  parse(char *line, char **argv)
 {
-     while (*line != '\0') {       /* if not the end of line ....... */ 
+     while (*line != '\0') {       /* if not the end of line ....... */
           while (*line == ' ' || *line == '\t' || *line == '\n')
                *line++ = '\0';     /* replace white spaces with 0    */
           *argv++ = line;          /* save the argument position     */
-          while (*line != '\0' && *line != ' ' && 
-                 *line != '\t' && *line != '\n') 
+          while (*line != '\0' && *line != ' ' &&
+                 *line != '\t' && *line != '\n')
                line++;             /* skip the argument until ...    */
      }
      *argv = '\0';                 /* mark the end of argument list  */
@@ -23,19 +24,18 @@ void  execute(char **argv)
      pid_t  pid;
      int    status;
 
-     if ((pid = fork()) < 0) {     /* fork a child process           */
+	 if ((pid = fork()) < 0) {     /* fork a child process           */
           printf("*** ERROR: forking child process failed\n");
           exit(1);
      }
      else if (pid == 0) {          /* for the child process:         */
-          if (execvp(*argv, argv) < 0) {     /* execute the command  */
-               printf("*** ERROR: exec failed\n");
-               exit(1);
-          }
+	   if(execvp(*argv, argv) < 0) {     /* execute the command  */
+			printf("*** ERROR: exec failed\n");
+			exit(1);
+		}
      }
      else {                                  /* for the parent:      */
-          while (wait(&status) != pid)       /* wait for completion  */
-               ;
+	   while(wait(&status) != pid);       /* wait for completion  */
      }
 }
 
@@ -46,7 +46,7 @@ void  main(void)
 
      while (1) {                   /* repeat until done ....         */
           printf("Shell -> ");     /*   display a prompt             */
-	  fgets(line, 1024, stdin);
+		  fgets(line, 1024, stdin);
 
           //gets(line);              /*   read in the command line     */
           printf("\n");
